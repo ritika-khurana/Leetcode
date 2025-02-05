c54a8c0c2664e25ca925fb1ab5d7b72223a0e48d
ritika <ritikakhurana451@gmai.com>
2025-02-05 18:44:19 +0530

    Added solution for problem XYZ

---
+++ 1790.py
@@ -0,0 +1,11 @@
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        if s1==s2:
+             return True
+        swap_idx=[i for i in range(len(s1))if s1[i]!=s2[i]]
+        if len(swap_idx)==2:
+             i,j=swap_idx
+             return s1[i] == s2[j] and s1[j] == s2[i]
+
+        return False
+        
